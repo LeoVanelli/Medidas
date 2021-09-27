662ac2d4b4de4c6e1a57091575c634a000f22c51
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:37:19 -0300

    Adiciona atividade Medidas

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Receba uma medida em metros e exiba seus equivalentes em quilômetros e centímetros.
+
+            Console.Write("Entre com a medida em metros: ");
+
+            double medida = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("----- Equivalência -----");
+            Console.WriteLine($"Centimetros: {medida*100} cm");
+            Console.WriteLine($"Metros: {medida} m");
+            Console.WriteLine($"QUilômetros: {medida/1000} km");
+        }
+    }
+}
